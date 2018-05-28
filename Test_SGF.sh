027c106f7dfc80b982e5729d01876d53b6313b0f
Yidi Zhao <Yidi Zhao>
2018-05-28 15:26:56 -0400

    May 28th 2

---
+++ Test_SGF.sh
@@ -0,0 +1,10 @@
+echo "./Test_SGF --betaMM 100  > betaMM100.txt"
+./Test_SGF --betaMM 100  > betaMM100.txt
+
+echo "./Test_SGF --betaMM 10  > betaMM10.txt"
+./Test_SGF --betaMM 10  > betaMM10.txt
+
+echo "./Test_SGF --betaMM 30  > betaMM30.txt"
+./Test_SGF --betaMM 30  > betaMM30.txt
+
+
